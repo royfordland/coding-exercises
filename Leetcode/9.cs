ca29c3a28b81e4db93b7e920d7b6d3ebaf99b2c4
royfordland <royneijs@outlook.com>
2026-02-16 13:17:54 -0500

    Update 9.cs - palindrome number

--- Leetcode/9.cs
+++ Leetcode/9.cs
@@ -2,30 +2,26 @@ public class Solution
 {
     public bool IsPalindrome(int x) 
     {
-        var result = x > 0 ? true : false;
+        var result = x >= 0 ? true : false;
         var stringNumber = x.ToString();
 
-        // I guess some Leetcode problems are either really length, or are like this 
-        // where the details are fairly minimal. A single number / character wouldn't
-        // be considered a palindrome, so let's skip that.
+        // 0 being a palindrome wasn't mentioned in the problem description but turned up as a test result
+        // faiing when submitting the code.
 
-        if(x > 0 && stringNumber.Length >= 2)
+        var rightIndex = stringNumber.Length - 1;
+
+        for(var index = 0; index <= rightIndex; index++)
         {
             // Check for palindrome by working from the very beginning and very end until 
             // we meet in the middle. Break out of the loop at the first occurrence of a mismatch.
-           
-            var rightIndex = stringNumber.Length - 1;
-
-            for(var index = 0; index <= rightIndex; index++)
+            
+            if(stringNumber[index] != stringNumber[rightIndex])
             {
-                if(stringNumber[index] != stringNumber[rightIndex])
-                {
-                    result = false;
-                    break;
-                }
-
-                rightIndex--;
+                result = false;
+                break;
             }
+
+            rightIndex--;
         }
         
         return result;  
