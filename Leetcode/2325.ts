cf07f442096976870cbc4f4c119c18223d038a10
royfordland <royneijs@outlook.com>
2026-02-16 11:02:04 -0500

    adding 2325

---
+++ Leetcode/2325.ts
@@ -0,0 +1,46 @@
+function decodeMessage(key: string, message: string): string {
+    const alphabet = "abcdefghijklmnopqrstuvwxyz";
+    key = key.replaceAll(" ", "");
+
+    const decryptValues = getDedupedKey(key);
+
+    let decryptedMsg = "";
+
+    for(const character of message)
+    {
+        if(character == " ")
+        {
+            decryptedMsg += " ";
+        }
+        else
+        {
+            // Get the letter from the map so we can get the decrypted message.
+            // Could be 1 line, but it's a bit easier to read when we don't have nested a[b[]]
+            const index = decryptValues.indexOf(character);
+
+            decryptedMsg += alphabet[index];
+        }
+    }
+
+    return decryptedMsg;
+};
+
+// In case we get duplicate values in the key we'll only want to pick on the first occurrence of a letter
+// Otherwise decrypting will be impossible, or at least very, very hard :P
+function getDedupedKey(key: string) 
+{
+    key = key.replaceAll(" ", "");
+    var result = new Array<string>();
+
+    for(const letter of key)
+    {
+        if(!result.includes(letter))
+        {
+            result.push(letter);
+        }
+    }
+
+    console.log('the result', result);
+
+    return result;
+}
