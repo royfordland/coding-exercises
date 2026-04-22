0abd1c71d676265cca4e31787d7fd7de1897c211
royfordland <royneijs@outlook.com>
2026-04-22 15:28:17 -0400

    67: add binary
    cleanup some

--- Leetcode/0067_add_binary.cs
+++ Leetcode/0067_add_binary.cs
@@ -2,25 +2,6 @@ using System;
 using System.Linq;
 using System.Text;
 
-//public class Solution
-//{
-//	public int[] PlusOne(int[] digits)
-//	{
-//		var stringNumber = string.Join("", digits);
-//		var number = BigInteger.Parse(stringNumber) + 1;
-//		stringNumber = number.ToString();
-
-//		var numbersArr = new int[stringNumber.Length];
-
-//		for (var index = 0; index < stringNumber.Length; index++)
-//		{
-//			numbersArr[index] = int.Parse(stringNumber[index].ToString());
-//		}
-
-//		return numbersArr;
-//	}
-//}
-
 public class Solution
 {
 	// My original solution was the following method below.
